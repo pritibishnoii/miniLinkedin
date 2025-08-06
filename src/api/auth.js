898803db9121f5b24a0217292a3a008f42ea32e5
priti <priti12bishnoi@gmail.com>
2025-08-06 20:10:35 +0530

    initial commit

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
-const USER_API_URL = 'http://localhost:5000/api/user';
+const API_URL = import.meta.env.VITE_API_URL + '/api/auth';
+const USER_API_URL = import.meta.env.VITE_API_URL + '/api/user';
 
 const signup = async (name, email, password, bio) => {
     const response = await axios.post(`${API_URL}/signup`, {
